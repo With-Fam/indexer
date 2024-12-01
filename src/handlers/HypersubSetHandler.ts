4ba804e522dbbea719d6edb3972830b77cb3991d
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-01 21:19:08 +0700

    I add network to event name.

--- src/handlers/HypersubSetHandler.ts
+++ src/handlers/HypersubSetHandler.ts
@@ -5,7 +5,7 @@ import { config } from "../utils/config";
 export async function handleHypersubSet(event: BaseEvent): Promise<void> {
   await trackEvents(event.network, [
     {
-      name: "HypersubSet",
+      name: `HypersubSet-${event.network}`,
       account: event.args.party,
       uniqueId: event.log.id,
       pointSystemId: config.stackPointSystemId,
