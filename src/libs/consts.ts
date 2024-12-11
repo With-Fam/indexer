a104ddf39925380eb18dbecbfc897fa637f7cb2d
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-11 10:32:19 -0500

    I remove unused consts file.

--- src/libs/consts.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from "viem";
-
-// TODO: Replace with actual address from deployment
-export const MANAGE_FAM_AUTHORITY_ADDRESS =
-  "0x0000000000000000000000000000000000000000" as Address;
